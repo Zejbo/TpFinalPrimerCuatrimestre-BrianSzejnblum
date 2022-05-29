9fa9f60434d409b1070582cfe1b2e831e4f13093
Brian sz <70424171+Zejbo@users.noreply.github.com>
2022-05-29 17:17:41 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed,fuerzaDeSalto ;
+    public Rigidbody rb;
+    public bool cuboEstaEnElPiso = true ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            // transform.position += new Vector3(0,0,0.1f);
+            transform.Translate(0, 0, movementSpeed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            // transform.position += new Vector3(0,0,0.1f);
+            transform.Translate(0, 0, -movementSpeed);
+        }
+        if(Input.GetButtonDown("Jump") && cuboEstaEnElPiso)
+            {
+            rb.AddForce(new Vector3(0, fuerzaDeSalto, 0), ForceMode.Impulse);
+            cuboEstaEnElPiso = false;
+        }
+    }
+    private void OnCollisionEnter(Collision Pepe)
+    {
+        if (Pepe.gameObject.name == "Piso")
+        {
+            cuboEstaEnElPiso = true;
+        }
+      
+    }
+}
